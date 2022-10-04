4cafd4d58856e29c23d17988ac87ac0cc452fcd4
Kanyshai Osmonova <kanyshai.osmonova.kk@gmail.com>
2022-10-04 15:29:05 +0600

    add CSS modules, React SKeletons and React Router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
 
 function App() {
-  const [items, setItems] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch('https://633b233e471b8c39557d3011.mockapi.io/items')
-      .then((res) => {
-        return res.json();
-      })
-      .then((arr) => {
-        setItems(arr);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock {...obj} key={obj.id} />
-            ))}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
         </div>
       </div>
     </div>
